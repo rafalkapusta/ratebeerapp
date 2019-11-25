42471809e229980a0ef98243789cd74d0346aba7
rafalkapusta <rfkapusta@gmail.com>
2019-11-25 16:55:25 +0100

    initial project files

---
+++ src/components/app/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Hello from "../hello/hello";
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <Hello/>
+          <p>TEST</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
