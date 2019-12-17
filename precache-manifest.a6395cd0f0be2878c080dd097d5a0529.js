16a5312e747c00d977662434d46817cb2f495fe2
rafalkapusta <rfkapusta@gmail.com>
2019-12-17 15:04:45 +0100

    Updates

---
+++ precache-manifest.a6395cd0f0be2878c080dd097d5a0529.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dbef287d7bf25f856e20a955e01843b",
+    "url": "/ratebeerapp/index.html"
+  },
+  {
+    "revision": "f079a3c7b17a85040857",
+    "url": "/ratebeerapp/static/css/main.18d08597.chunk.css"
+  },
+  {
+    "revision": "3a5eee8136613f8a2651",
+    "url": "/ratebeerapp/static/js/2.6332f17c.chunk.js"
+  },
+  {
+    "revision": "f079a3c7b17a85040857",
+    "url": "/ratebeerapp/static/js/main.7085e0c5.chunk.js"
+  },
+  {
+    "revision": "b25355968ae56bb8d500",
+    "url": "/ratebeerapp/static/js/runtime-main.b2cad65b.js"
+  },
+  {
+    "revision": "99e710bb1fb0dea99fc0735aeb12f925",
+    "url": "/ratebeerapp/static/media/background.99e710bb.jpg"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/ratebeerapp/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/ratebeerapp/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "a9c4bb7348f42626454c988dbde1d0a0",
+    "url": "/ratebeerapp/static/media/fa-brands-400.a9c4bb73.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/ratebeerapp/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/ratebeerapp/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/ratebeerapp/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/ratebeerapp/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/ratebeerapp/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/ratebeerapp/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "c2801fb415f03c7b170934769d7b5397",
+    "url": "/ratebeerapp/static/media/fa-solid-900.c2801fb4.svg"
+  }
+]);
